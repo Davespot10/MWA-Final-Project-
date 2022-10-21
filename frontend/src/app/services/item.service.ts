5ceaa4eb4e1785f17288981122f788a747bdf67a
Belay Buta <belaynehmulat@gmail.com>
2022-10-21 16:31:16 -0500

    service and reactive form implementation for the item component

---
+++ frontend/src/app/services/item.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Item } from '../models/item.model';
+import {Observable} from 'rxjs'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+   BASE_URL="http://localhost:3000/";  // to be moved to environmental variable
+
+  constructor(private http:HttpClient) { }
+
+  postItem(item:Item):Observable<Item>{
+    return this.http.post<Item>(this.BASE_URL, item);
+
+  }
+  getItems():Observable<Item[]>{
+    return this.http.get<Item[]>(this.BASE_URL);
+  }
+  getItemById(id:string):Observable<Item>{
+    return this.http.get<Item>(this.BASE_URL +id);
+  }
+  searchItems(qParams:any, skip:any){
+    // search to be implemented here
+
+  }
+  updateItem(id:string, item:Item):Observable<Item>{
+    return this.http.patch<Item>(this.BASE_URL+id,item)
+  }
+  deleteItem(id:string):Observable<Item>{
+    return this.http.delete<Item>(this.BASE_URL);
+
+  }
+}
