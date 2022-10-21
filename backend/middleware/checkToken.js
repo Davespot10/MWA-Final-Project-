c831cb0940d4e1eb32002f78e61307f5dad9cc64
Davespot10 <davespot10@gmail.com>
2022-10-21 08:41:21 -0500

    middleware done for signin and login

---
+++ backend/middleware/checkToken.js
@@ -0,0 +1,11 @@
+const jsonwebtoken = require("jsonwebtoken");
+
+module.exports.checkToken = ((res, req, next) => {
+    const token = req.headers.authorization.split(" ")[1];
+    const decod=jsonwebtoken.verify(token,'LOST_AND_FOUND_SECRET')
+    if (!decod) {
+        next(new Error("Invalid token"))
+    }
+    next();
+    
+})
