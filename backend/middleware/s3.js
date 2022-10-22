8e8a1660dab1520e3154350e3ee9798583b4c9d7
HasibRashid95 <108424030+HasibRashid95@users.noreply.github.com>
2022-10-22 18:50:31 -0500

    update s3 to upload directly to the aws s3
    need to pass the image file to get a url

--- backend/middleware/s3.js
+++ backend/middleware/s3.js
@@ -1,5 +1,8 @@
+
+import crypto from 'crypto'
 const S3 = require('aws-sdk/clients/s3');
 require('dotenv').config();
+const randomBytes = promisify(crypto.randomBytes);
 
 const bucketName =process.env.AWS_S3_BUCKET_NAME;
 const region =process.env.AWS_S3_BUCKET_REGION;
@@ -11,3 +14,25 @@ const s3 = new S3({
     accessKey,
     secretKey
 });
+
+
+export async function  uploadFile(file) {
+    const rawBytes = await randomBytes(16)
+    const imageName = rawBytes.toString('hex')
+    var params = { 
+        Bucket: bucketName, 
+        Key: imageName, 
+        Expires: 3600,
+        ContentType:file.data.type };
+    var url= s3.getSignedUrl('putObject',params);
+     this.http.put(url, file.data).subscribe((res) => {
+        console.log(res)
+       
+        
+    })
+
+    return url;
+  }
+
+
+  
