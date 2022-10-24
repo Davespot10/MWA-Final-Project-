1103e0acf3122facefa416ff5a10b2cdde206cb9
Belay Buta <belaynehmulat@gmail.com>
2022-10-24 14:15:10 -0500

    address autocomplete implementation ui

--- backend/utils/geocoder.js
+++ backend/utils/geocoder.js
@@ -3,8 +3,6 @@ const NodeGeocoder = require('node-geocoder');
 
 const options = {
   provider: process.env.GEOCODER_PROVIDER,
-
-  fetch: customFetchImplementation,
   apiKey: process.env.GEOCODER_API_KEY, 
   formatter: null 
 };
